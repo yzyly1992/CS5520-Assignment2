30ae600da22d44ec0da4f5322e800fcda45b6e98
David Yang <zyang03@risd.edu>
2023-02-14 10:59:15 -0800

    initialize 4 screens

---
+++ screens/AllEntries.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AllEntries() {
+  return (
+    <View>
+      <Text>AllEntries</Text>
+    </View>
+  )
+}
---
+++ screens/EditEntry.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function EditEntry() {
+  return (
+    <View>
+      <Text>EditEntry</Text>
+    </View>
+  )
+}
