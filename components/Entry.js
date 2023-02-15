1d3a8ea5ade86926d082d9239c3bd24c20ea6b89
David Yang <zyang03@risd.edu>
2023-02-14 16:11:56 -0800

    create Button Card Entry EntryList Input Color components

---
+++ components/Entry.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Entry() {
+  return (
+    <View>
+      <Text>Entry</Text>
+    </View>
+  )
+}
---
+++ components/Input.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Input() {
+  return (
+    <View>
+      <Text>Input</Text>
+    </View>
+  )
+}
